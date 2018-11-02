6399109fd2bf87e4f8412c006d19e65310dba80e
Ethan McWilliams <ethan.mcwilliams@pharmacy2u.co.uk>
2018-11-02 09:27:40 +0000

    Moved RabbitMQ projects to a new folder
    This is so I can prototype a remote version with terraform and DO, and keep a local version too

---
+++ Projects/RabbitMQ/Local/Read/Read.cs
@@ -0,0 +1,27 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Threading;
+
+internal class Receive
+{
+    public static void Main()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(
+                queue: "downloader",
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+            while (true)
+            {
+                Console.Clear();
+                Console.Write($"Current Message Count: {channel.MessageCount(queue: "downloader")}");
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
